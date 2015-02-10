e6499bc0ce05b905ec15057d76b757ab10bcd7a6
Danny Wu <wushuo1988@hotmail.com>
2015-02-09 23:06:15 -0500

    Use Devise for user authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Needed for Devise
+  config.action_mailer.default_url_options = { host: 'www.1xuer.com' }
 end
